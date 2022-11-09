9be55ff8fd43e1344079bd77a24b683921f253be
livier aguillon <livieralvarez@gmail.com>
2022-11-08 20:59:29 -0600

    saving property assignment

---
+++ Assignments/propertychange.py
@@ -0,0 +1,28 @@
+#Assignnemt
+# Display 3 properties
+# Allow the user to change one of the properties using the key
+# System will generate a random property and display all 3 properties again
+''' Initial Properties will be randomly generated
+1 Strength 210
+2 Armor 156
+3 Resistance 95
+
+Which property would you like to change?
+1
+
+1 Life Steal 5%
+2 Armor 156
+3 Resistance 95
+
+Loop until user is happy
+git commands to save files
+1. make sure you are in the root pythin folder
+2. git status - tell us about any changes in our directory
+3. git add [File] - add file to our list of items to be commited
+4. git status - should show all our changes in green
+5. git commit -m"message for commit"
+6. git push - sends the data to the remote repository
+
+Use notes from 10.24.22 for random
+'''
+
